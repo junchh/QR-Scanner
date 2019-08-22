948c97f1febd66ba6413cfd83bfd1ee51584648d
Jun <cjunho9868@gmail.com>
2019-08-22 13:29:54 +0700

    hey

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 </head>
+<style>
+#preview
+{
+    transform: rotateY(180deg);
+    -webkit-transform:rotateY(180deg); /* Safari and Chrome */
+    -moz-transform:rotateY(180deg); /* Firefox */
+}
+</style>
 <body>
   
     <h1>ARC Scanner</h1>
